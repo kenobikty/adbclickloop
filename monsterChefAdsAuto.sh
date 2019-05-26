a787c4c0193f124b476447927152d37c2da3efa1
kenobi <>
2019-05-26 18:37:21 +0800

    first commit

---
+++ monsterChefAdsAuto.sh
@@ -0,0 +1,12 @@
+while true
+do
+adb shell input tap 1004 234
+adb shell input tap 814 1251
+sleep 35
+adb shell input tap 104 135
+adb shell input tap 1020 135
+sleep 2
+adb shell input tap 1020 135
+sleep 5
+done
+
