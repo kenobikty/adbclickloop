243505c296dc604ab9ac205f5436da6243a63c39
kenobi <ken.ktyken10@gmail.com>
2020-11-15 12:44:37 +0800

    fixed variable and added timestamp

--- adbAuto.sh
+++ adbAuto.sh
@@ -1,10 +1,13 @@
-x=$((235 + (RANDOM % 150) + 1))
-y=$((960 + (RANDOM % 20) + 1))
-t=$((5 + (RANDOM % 5) + 1))
+timestamp() {
+  date +"%T" # current time
+}
 while
   true
 do
-  echo "Running"
+  x=$((235 + (RANDOM % 150) + 1))
+  y=$((960 + (RANDOM % 20) + 1))
+  t=$((7 + (RANDOM % 3) + 1))
+  echo $(timestamp)
   adb shell input tap $x $y
   sleep 3
   adb shell input tap $x $y
